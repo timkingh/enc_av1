be16d656d2cec68b4f4be4bf9451da94957b61ed
timkingh <timkingh.huang@rock-chips.com>
2023-08-30 14:37:13 +0800

    Update debug cfg files
    Signed-off-by: timkingh <timkingh.huang@rock-chips.com>

--- .vscode/rkbuild.sh
+++ .vscode/rkbuild.sh
@@ -1,1 +1,16 @@
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCONFIG_AV1_ENCODER=0 -DCONFIG_AV1_DECODER=1 -DAOM_TARGET_CPU=generic -DCONFIG_MULTITHREAD=0 -DENABLE_DOCS=0 -DCMAKE_CXX_COMPILER=C:\Strawberry\c\bin\gcc.exe
+
+mkdir -p rkbuild
+
+cd rkbuild || exit
+
+if [[ ! -a aomdec ]]; then
+    cmake .. -DCMAKE_BUILD_TYPE=Debug \
+             -DCONFIG_AV1_ENCODER=0 \
+             -DCONFIG_AV1_DECODER=1 \
+             -DAOM_TARGET_CPU=generic \
+             -DCONFIG_MULTITHREAD=0 \
+             -DENABLE_DOCS=0 
+             #-DCMAKE_CXX_COMPILER=/usr/bin/gcc
+fi
+
+make -j32
